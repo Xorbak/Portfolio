a485470a6dc5f63e0b6967eebf74cf108f760a69
dewald.labuschagne <dewaldstar@gmail.com>
2022-09-27 15:57:51 +0200

    added weather to Navbar

--- src/components/Navbar/responsiveNavBar.tsx
+++ src/components/Navbar/responsiveNavBar.tsx
@@ -12,15 +12,71 @@ import { TaskDropdown } from "./components/taskDropdown";
 import { HomeButton } from "./components/homeButton";
 import { ThemeDropDown } from "./components/themeDropdown";
 import { Theme, ThemeOptions } from "@mui/material/styles";
+import { CurrentWeatherData, Geolocation } from "../../App";
+import { useEffect } from "react";
 
 //Refactor, works but its messy
 const pages = ["Home", "Tasks", "Blog"];
 //const settings = ["Profile", "Account", "Dashboard", "Logout"];
+
 interface Props {
   setCurrentTheme: React.Dispatch<React.SetStateAction<Theme>>;
+  setGeolocation: React.Dispatch<React.SetStateAction<Geolocation | undefined>>;
+  setWeatherData: React.Dispatch<
+    React.SetStateAction<CurrentWeatherData | undefined>
+  >;
+  geolocation: Geolocation | undefined;
+  weatherData: CurrentWeatherData | undefined;
 }
 
-export const ResponsiveAppBar = ({ setCurrentTheme }: Props) => {
+export const ResponsiveAppBar = ({
+  setCurrentTheme,
+  setGeolocation,
+  setWeatherData,
+  geolocation,
+  weatherData,
+}: Props) => {
+  //--------------------weather forcast
+  const apiKey = "0d004acbd263f70a7810a4c700aff384";
+  //------ reverse geocoding url << not working too well
+
+  const cityApiUrl = `http://api.openweathermap.org/geo/1.0/reverse?lat=${geolocation &&
+    geolocation.latitude}&lon=${geolocation &&
+    geolocation.longitude}&limit=5&appid=0d004acbd263f70a7810a4c700aff384`;
+
+  //https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key} <<<< API call using city name instead of lon lat
+  //https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid={API key} <<<<<< API call using lon lat
+
+  const weatherApiUrl = `https://api.openweathermap.org/data/2.5/weather?units=metric&lat=${geolocation &&
+    geolocation.latitude}&lon=${geolocation &&
+    geolocation.longitude}&appid=${apiKey}`;
+  //--------------------------------------------
+  const imgUrl = `http://openweathermap.org/img/wn/${weatherData &&
+    weatherData.weather[0].icon}.png`;
+  //--------------------------------------------------
+  useEffect(() => {
+    const fetchCity = async () => {
+      navigator.geolocation.getCurrentPosition((position) => {
+        setGeolocation({
+          longitude: position.coords.longitude,
+          latitude: position.coords.latitude,
+        });
+      });
+      console.log(geolocation);
+      geolocation &&
+        (await fetch(weatherApiUrl)
+          .then((res) => res.json())
+          .then((result) => {
+            setWeatherData(result);
+          }));
+    };
+
+    fetchCity();
+  }, [
+    geolocation && geolocation.longitude,
+    geolocation && geolocation.latitude,
+  ]);
+
   //Handles opening and closing of dropdowns
   const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(
     null
@@ -32,18 +88,7 @@ export const ResponsiveAppBar = ({ setCurrentTheme }: Props) => {
   const handleCloseNavMenu = () => {
     setAnchorElNav(null);
   };
-  //code for user menu might want to use it later
-  /*const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(
-    null
-  );
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
-  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElUser(event.currentTarget);
-  };*/
 
-  //when mapping through "pages" array this allows me to render components
   const menuItem = (i: any) => {
     let component;
     switch (i) {
@@ -70,7 +115,7 @@ export const ResponsiveAppBar = ({ setCurrentTheme }: Props) => {
       <Container sx={styles.navbar} maxWidth="xl">
         <Toolbar disableGutters>
           <Box
-            //mobile size menu
+            //--------------------------------mobile size menu
             sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}
           >
             <IconButton
@@ -108,9 +153,8 @@ export const ResponsiveAppBar = ({ setCurrentTheme }: Props) => {
               ))}
             </Menu>
           </Box>
-
           <Box
-            //mobile and up navbar
+            //------------------mobile and up navbar
             sx={{
               flexGrow: 1,
               display: { xs: "none", md: "flex" },
@@ -127,6 +171,30 @@ export const ResponsiveAppBar = ({ setCurrentTheme }: Props) => {
               </Typography>
             ))}
           </Box>
+          {weatherData && (
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "row",
+                textAlign: "center",
+
+                alignItems: "center",
+              }}
+            >
+              <Box>
+                <img src={imgUrl} height={50} width={50} />
+              </Box>
+              <Box sx={{ display: "flex", flexDirection: "column" }}>
+                <Typography variant="subtitle1">
+                  {Math.round(weatherData.main.feels_like)}°C
+                </Typography>
+                <Typography variant="caption">
+                  {Math.round(weatherData.main.temp_max)}°C/
+                  {Math.round(weatherData.main.temp_min)}°C
+                </Typography>
+              </Box>
+            </Box>
+          )}
         </Toolbar>
       </Container>
     </AppBar>
