4a0f32815b19418bf5d238cb7c63da29bf62e7da
dewald.labuschagne <dewaldstar@gmail.com>
2023-02-18 18:03:53 +0200

    fixed image input

--- src/components/AiImage/AiImage.tsx
+++ src/components/AiImage/AiImage.tsx
@@ -11,17 +11,17 @@ import axios from "axios";
 
 export const AiImage = () => {
   const [searchInput, setSearchInput] = useState<string>("");
-  const [imageResult, setImageResult] = useState<string>("");
+  const [imageResult, setImageResult] = useState<string>();
   const [loading, setLoading] = useState<boolean>(false);
   const [submitted, setSubmitted] = useState<boolean>(false);
   const [errorCode, setErrorCode] = useState<undefined | string>();
   const config = new Configuration({ apiKey: process.env.REACT_APP_OPEN }); //used to get the API from the .env
   const openAi = new OpenAIApi(config); //setting up the new instance of OpenAIApi
 
-  const getImage = (prompt: string) => {
+  const getImage = (prompt: string, input: string) => {
     const options = {
       method: "GET",
-      url: "https://xorprod.herokuapp.com/image",
+      url: `http://localhost:5000/${input}`,
       params: { input: prompt },
     };
     axios
@@ -30,9 +30,9 @@ export const AiImage = () => {
         console.log(res.data.data[0].url);
         setImageResult(res.data.data[0].url ? res.data.data[0].url : "");
       })
-      .catch((e) => setErrorCode(e.response.data));
+      .catch((e) => setErrorCode(e.response));
   };
-  console.log(imageResult);
+
   return (
     <Grid sx={styles.App}>
       <Grid
@@ -48,7 +48,7 @@ export const AiImage = () => {
         <Formik
           initialValues={{ prompt: "", loading: false }}
           onSubmit={(values, { resetForm }) => {
-            setLoading(true), getImage(values.prompt);
+            setLoading(true), getImage(values.prompt, "image");
             setSearchInput(values.prompt), resetForm();
           }}
           validationSchema={imageValidation}
@@ -88,7 +88,7 @@ export const AiImage = () => {
           }}
         </Formik>
         {loading == true ? (
-          imageResult.length > 0 ? (
+          imageResult && imageResult.length > 0 ? (
             <React.Fragment>
               <Typography
                 sx={{ marginY: "5px", marginX: { xs: "10px", md: 0 } }}
@@ -100,7 +100,7 @@ export const AiImage = () => {
                 container
                 component="img"
                 sx={{ width: { xs: "95vw", md: "300px" } }}
-                src={imageResult}
+                src={imageResult && imageResult}
               />
             </React.Fragment>
           ) : errorCode == undefined ? (
--- src/components/AiImage/AiInput.tsx
+++ src/components/AiImage/AiInput.tsx
@@ -2,5 +2,16 @@ import TextField from "@mui/material/TextField";
 import React from "react";
 //@ts-ignore
 export const AiInput = ({ field, form, ...props }) => {
-  return <TextField size="small" autoComplete="off" {...field} {...props} />;
+  return (
+    <TextField
+      maxRows={5}
+      minRows={3}
+      inputProps={{ maxLength: 50 }}
+      multiline
+      size="small"
+      autoComplete="off"
+      {...field}
+      {...props}
+    />
+  );
 };
