2203399559ffb60518979bd266557701c050f1ad
dewald.labuschagne <dewaldstar@gmail.com>
2023-02-25 18:42:12 +0200

    Updates

---
+++ precache-manifest.daeef96fbbe74277013a39b1336a74d7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a94ff0f49d38add37077",
+    "url": "/Portfolio/static/css/main.25cdd0d2.chunk.css"
+  },
+  {
+    "revision": "a94ff0f49d38add37077",
+    "url": "/Portfolio/static/js/main.a94ff0f4.chunk.js"
+  },
+  {
+    "revision": "259a51d14fc01d925024",
+    "url": "/Portfolio/static/js/1.259a51d1.chunk.js"
+  },
+  {
+    "revision": "188a3269e3f1431616ac",
+    "url": "/Portfolio/static/js/2.188a3269.chunk.js"
+  },
+  {
+    "revision": "c08e47195d5c3eed7100",
+    "url": "/Portfolio/static/js/runtime~main.c08e4719.js"
+  },
+  {
+    "revision": "39b83f719689417e2de3b59261f8cf9e",
+    "url": "/Portfolio/static/media/bun.39b83f71.jpg"
+  },
+  {
+    "revision": "ba2f393db332af740e2cde1cff4bd682",
+    "url": "/Portfolio/index.html"
+  }
+];
